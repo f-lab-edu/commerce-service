bb246fef04f2d69b8523d49e9582ea0de4a0ee4b
today8934 <today8934@gmail.com>
2021-09-14 10:27:15 +0900

    First Commit

---
+++ src/main/java/study/commerceservice/CommerceServiceApplication.java
@@ -0,0 +1,13 @@
+package study.commerceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommerceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommerceServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/study/commerceservice/CommerceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package study.commerceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommerceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
